1ff6e712ae3cd12ef6aa70a3b38d01ab51a02838
showemeowkx <sashacherepov55@gmail.com>
2025-03-24 09:42:32 +0100

    Add better execution examples

--- Pt2/lab1/index.js
+++ Pt2/lab1/index.js
@@ -1,15 +1,23 @@
 // SETTING UP TUTORIAL
 // First argument (type: number) - the length of a generated srting.
-// Giving a negative value will generate strings with random length in the range of 1 to a positive value of the one passed.
+// Giving a negative value will generate strings with random length in the range from 1 to a positive value of the one passed.
 // Second argument (type: string) - the string of characters from which the random string is formed.
 // Third argument (type: number) - the timeout duration in ms.
 
-// Execution example:
+// Execution examples:
 const iterator = require("./utils/iterator");
 const CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-const iterable = iterator(7, CHARS, 1000);
+const iterableFixedLen = iterator(7, CHARS, 30);
+const iterableRandomLen = iterator(-10, "01", 30);
 
-for (const str of iterable) {
-  console.log(str);
-}
+const iterate = (iterable) => {
+  return new Promise((resolve) => {
+    for (const str of iterable) {
+      console.log(str);
+    }
+    resolve();
+  });
+};
+
+iterate(iterableFixedLen).then(() => iterate(iterableRandomLen));
